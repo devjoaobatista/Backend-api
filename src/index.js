b23320a025d8aa5e680a003ebab539a3da1ef96e
João <dev.joaobatista@gmail.com>
2022-12-22 00:12:26 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// 
+
+import express from 'express';
+const app = express()
+
+app.post('/users', (req, res) => {
+    return res.send("Usuário criado");
+})
+
+app.get('/users', (req, res) => {
+    return res.send("retornar o usuário");
+});
+
+app.delete('/users', (req, res) => {
+    return res.send("Usuário deletado");
+});
+
+app.put('users', (req, res) => {
+    return res.send("Usuário atualizado");
+});
+
+app.listen(5000, () => {
+    return console.log ("Servidor rodando");
+});
+
+
+
+
+
