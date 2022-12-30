11ddad0972f4a4d9d574c2bc37ed0fe4bd6fa62b
João <dev.joaobatista@gmail.com>
2022-12-30 00:18:49 -0300

    Add all routes and middleware

--- src/index.js
+++ src/index.js
@@ -1,45 +1,71 @@
-import express from 'express';
-const app = express()
-app.use(express.json())
+import express, { query } from 'express';
+const app = express();
+app.use(express.json());
 
-//O ID dos "users" é o indice do array
-const allusers = [
-    {nome: "joao", telefone: 12345678, email: "joao@express.com", endereco: "Vila Express, n8" },
-    {nome: "Maria", telefone: 17777777, email: "maria@express.com", endereco: "Vila Express, n9" },
-    {nome: "Pedro", telefone: 22222222, email: "pedro@express.com", endereco: "Vila Express, n10" },    
+const users = [
+    {nome: "Joao", mail: "joao@js.com", fone: 9999999, endereco: "rua 2"},
+    {nome: "Pedro", mail: "pedro@js.com", fone: 77777777, endereco: "rua 1"}
 ];
 
-app.post('/users', (req, res) => {
-    let newuser = req.body
-    allusers.push(newuser);
-    console.log(allusers);
-    return res.send("Usuário criado");
+function logRoutes(request, response, next) {
+    const {method, url } = request;
+    const route = `[${method.toUpperCase()}] ${url}`;
+    console.log(route);
+    return next();
+};
+
+app.use(logRoutes);
+
+app.get("/server", (request, response) => {
+    const usersWithIndex = users.map((user, id) => {
+        const objectTransformed = { id, ...user };
+        return  objectTransformed;
+    });    
+    return response.send(usersWithIndex);
 });
 
-app.get('/users', (req, res) => {
-    return res.send(allusers);
-   
- //tentativa de retorna o indice do array como id.
- // for(let i = 0; i < allusers.length; i++){
-   //     return res.send(i + allusers[i]);
-   // }
+app.get("/server/:id", (request, response) => {
+    const { id } = request.params;
+    
+    if(id > users.length) {
+        return response.status(404).send("Esse ID não existe!");
+    }
+    else {
+        return response.send(users[id]);
+    }
 });
 
-app.delete('/users', (req, res) => {
-    return res.send("Usuário deletado");
+app.post("/server", (request, response) => {
+    let newUser = request.body;
+    users.push(newUser);
+
+    return response.status(201).send("Usuário criado!");
 });
 
-app.put('/users/:id', (req, res) => {
-    const params = req.params
-    console.log(params)
-    return res.send("Usuário atualizado");
+app.put("/server/:id", (request, response) => {
+    const { id } = request.params;
+    const updateUser = request.body;
+    
+    if(id > users.length) {
+        return response.status(404).send("Esse ID não existe");
+    }
+    else {
+        users[id] = updateUser
+        return response.send("Usuário atualizado!");
+    }
 });
 
-app.listen(5000, () => {
-    return console.log ("Servidor rodando");
+app.delete("/server/:id", (request, response) => {
+    const { id } = request.params;
+    
+    if(id > users.length) {
+        return response.status(404).send("Esse ID não existe!");
+    }
+    else {
+        users.splice(id, 1);
+        return response.send("Usuário deletado!");
+    }
 });
 
 
-
-
-
+app.listen(3000, () => { return console.log ("Servidor rodando"); });
