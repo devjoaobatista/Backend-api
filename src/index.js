8880da39919741446056d4ffb0b0b2c75c6bc1bf
João <dev.joaobatista@gmail.com>
2023-01-02 09:44:31 -0300

    routes completed

--- src/index.js
+++ src/index.js
@@ -1,71 +1,68 @@
 import express from 'express';
+
 const app = express();
 app.use(express.json());
 
 const users = [
-    {nome: "Joao", mail: "joao@js.com", fone: 9999999, endereco: "rua 2"},
-    {nome: "Pedro", mail: "pedro@js.com", fone: 77777777, endereco: "rua 1"}
+  {
+    nome: 'Joao', mail: 'joao@js.com', fone: 9999999, endereco: 'rua 2',
+  },
+  {
+    nome: 'Pedro', mail: 'pedro@js.com', fone: 77777777, endereco: 'rua 1',
+  },
 ];
 
-function logRoutes(request, response, next) {
-    const {method, url } = request;
-    const route = `[${method.toUpperCase()}] ${url}`;
-    console.log(route);
-    return next();
-};
-
-app.use(logRoutes);
-
-app.get("/server", (request, response) => {
-    const usersWithIndex = users.map((user, id) => {
-        const objectTransformed = { id, ...user };
-        return  objectTransformed;
-    });    
-    return response.send(usersWithIndex);
+// Esse get retorna todos os usuários.
+app.get('/server', (request, response) => {
+  const usersWithIndex = users.map((user, id) => {
+    const objectTransformed = { id, ...user };
+    return objectTransformed;
+  });
+  return response.send(usersWithIndex);
 });
 
-app.get("/server/:id", (request, response) => {
-    const { id } = request.params;
-    
-    if(id > users.length) {
-        return response.status(404).send("Esse ID não existe!");
-    }
-    else {
-        return response.send(users[id]);
-    }
+// Esse get retorna apenas um usuário através do ID repassado.
+app.get('/server/:id', (request, response) => {
+  const { id } = request.params;
+
+  if (id > users.length) {
+    return response.status(404).send('Esse ID não existe!');
+  }
+
+  return response.send(users[id]);
 });
 
-app.post("/server", (request, response) => {
-    let newUser = request.body;
-    users.push(newUser);
+// Esse post cria um novo usuário
+app.post('/server', (request, response) => {
+  const newUser = request.body;
+  users.push(newUser);
 
-    return response.status(201).send("Usuário criado!");
+  return response.status(201).send('Usuário criado!');
 });
 
-app.put("/server/:id", (request, response) => {
-    const { id } = request.params;
-    const updateUser = request.body;
-    
-    if(id > users.length) {
-        return response.status(404).send("Esse ID não existe");
-    }
-    else {
-        users[id] = updateUser
-        return response.send("Usuário atualizado!");
-    }
+// Esse put atualiza um usuário da ID repassada.
+app.put('/server/:id', (request, response) => {
+  const { id } = request.params;
+  const updateUser = request.body;
+
+  if (id > users.length) {
+    return response.status(404).send('Esse ID não existe');
+  }
+
+  users[id] = updateUser;
+  return response.send('Usuário atualizado!');
 });
 
-app.delete("/server/:id", (request, response) => {
-    const { id } = request.params;
-    
-    if(id > users.length) {
-        return response.status(404).send("Esse ID não existe!");
-    }
-    else {
-        users.splice(id, 1);
-        return response.send("Usuário deletado!");
-    }
+// Esse delete apaga o usuário da ID repassada.
+app.delete('/server/:id', (request, response) => {
+  const { id } = request.params;
+
+  if (id > users.length) {
+    return response.status(404).send('Esse ID não existe!');
+  }
+
+  users.splice(id, 1);
+  return response.send('Usuário deletado!');
 });
 
-
-app.listen(3000, () => { return console.log ("Servidor rodando"); });
+app.listen(3000, () => console.log('Servidor rodando'));
